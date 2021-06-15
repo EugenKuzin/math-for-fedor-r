565c6c9af0caccb82947a4b0ee5420afd6cec665
fedor <teodantes@gmail.com>
2021-06-15 20:06:45 +0300

    Change tuple to list

--- canonical_representation_of_positive_integer.py
+++ canonical_representation_of_positive_integer.py
@@ -1,34 +1,23 @@
-from collections import Iterable # для определения функции flatten()
-from sys import argv # для передачи аргументов скрипту прямо в командной строке
-
-def flatten(lis): # рекурсивным методом делает из вложенных списков одноуровневый список
-     for item in lis:
-         if isinstance(item, Iterable) and not isinstance(item, str):
-             for x in flatten(item):
-                 yield x
-         else:
-             yield item
-
 def can_repr(x):
-    '''Функция раскладывает число на простые множители, которые выводит в виде кортежа.'''
-    multipliers = []
-    if x <= 0:
-        print('Введи положительное число:')
-        x = int(input('>>> '))
+    global multipliers
     if x == 1:
         return 'Единица не является простым числом'
     else:
         for i in range (x - 1, 0, -1):
             if x % i == 0:
-                multipliers.append(x // i)
+                multipliers += [x // i]
                 temp = i
                 break
-        if temp == 1:
-            return tuple(list(flatten(multipliers)))
+        if temp > 1:
+            print(multipliers)
+            print(temp)
+            multipliers += [can_repr(temp)]
+            return multipliers
         else:
-            multipliers.append(can_repr(temp))
-            return tuple(list(flatten(multipliers)))
+            print('end:', multipliers)
+            print(temp)
+            return multipliers
 
-script, number = argv # берем все содержимое argv, распаковываем его и назначаем в качестве значений переменных по порядку слева
+multipliers = []
 
-print(can_repr(int(number))) # передаем аргумент функции
+print(can_repr(int(input())))
