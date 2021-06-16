6cf51a9bff6d23c4c3b1b0b924d9dcfd9f5b12e2
fedor <teodantes@gmail.com>
2021-06-16 20:31:09 +0300

    Add ol version of the function

--- canonical_representation_of_positive_integer.py
+++ canonical_representation_of_positive_integer.py
@@ -3,19 +3,15 @@ def can_repr(x):
     if x == 1:
         return 'Единица не является простым числом'
     else:
-        for i in range (x - 1, 0, -1):
+        for i in range(x - 1, 0, -1):
             if x % i == 0:
                 multipliers += [x // i]
                 temp = i
                 break
         if temp > 1:
-            print(multipliers)
-            print(temp)
             multipliers += [can_repr(temp)]
             return multipliers
         else:
-            print('end:', multipliers)
-            print(temp)
             return multipliers
 
 multipliers = []
