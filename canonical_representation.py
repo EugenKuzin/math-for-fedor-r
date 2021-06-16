54b5bcb7bfab5d228100e4e7d53f24a39e3d769a
fedor <teodantes@gmail.com>
2021-06-16 20:25:52 +0300

    Add new version of the function

---
+++ canonical_representation.py
@@ -0,0 +1,15 @@
+def can_repr(x):
+    result = []
+    if x <= 0:
+        return 'Необходимо ввести положительное число!'
+    if x == 1:
+        return 'Единица не является простым числом!'
+    for i in range(x - 1, 1, -1):
+        if x % i == 0:
+            result += [x // i]
+            x = i
+    result += [x]
+    return result
+
+
+print(can_repr(int(input())))
