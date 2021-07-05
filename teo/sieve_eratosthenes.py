8f7c7ebafceae8df781c27de2dec70c9f4de74e1
teo <teodantes@gmail.com>
2021-07-05 15:29:40 +0300

    Fix the script
    Changed the value of an inner for loop and added the checking on belonging to the set of not-primes numbers:

--- teo/sieve_eratosthenes.py
+++ teo/sieve_eratosthenes.py
@@ -10,9 +10,10 @@ def sieve(x):
         result = [2]
         not_primes = set()
         for i in range(3, x + 1, 2):
-            for k in range(i ** 2, x + 1, 2):
-                if k % i == 0:
-                    not_primes.add(k)
+            if i not in not_primes:
+                for k in range(i ** 2, x + 1, i):
+                    if k % i == 0:
+                        not_primes.add(k)
         for i in range(3, x + 1, 2):
             if i not in not_primes:
                 result.append(i)
