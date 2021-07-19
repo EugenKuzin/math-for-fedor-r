1a0ccf65f28e0102447d57ff6cefb233661615c8
teo <teodantes@gmail.com>
2021-07-20 01:38:59 +0300

    Edit Addition

--- teo/addition.py
+++ teo/addition.py
@@ -1,17 +1,22 @@
 #!/usr/bin/env python3
 
 def successor(x):
+    '''Print the next integer.'''
     return x + 1
 
 def predecessor(x):
+    '''Print the previous integer.'''
     return x - 1
 
 def sum(a, b):
+    '''Print the sum of given integers.'''
     return a if b == 0 else successor(sum(a, predecessor(b)))
 
 def main():
-    a = int(input())
-    b = int(input())
+    print('Enter two integers you want to sum...')
+    a = int(input('> '))
+    b = int(input('> '))
+    print('Result:')
     print(sum(a, b))
 
 if __name__ == '__main__':
