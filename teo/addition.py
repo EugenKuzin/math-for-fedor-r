778fc810446fc66e211a38c27f9d469e73964f24
teo <teodantes@gmail.com>
2021-07-20 01:47:27 +0300

    Edit Addition

--- teo/addition.py
+++ teo/addition.py
@@ -1,15 +1,21 @@
 #!/usr/bin/env python3
 
-def successor(x):
-    '''Print the next integer.'''
+def successor(x: int) -> int:
+    '''
+    Print the next integer.
+    '''
     return x + 1
 
-def predecessor(x):
-    '''Print the previous integer.'''
+def predecessor(x: int) -> int:
+    '''
+    Print the previous integer.
+    '''
     return x - 1
 
-def sum(a, b):
-    '''Print the sum of given integers.'''
+def sum(a, b: int) -> int:
+    '''
+    Print the sum of given integers.
+    '''
     return a if b == 0 else successor(sum(a, predecessor(b)))
 
 def main():
