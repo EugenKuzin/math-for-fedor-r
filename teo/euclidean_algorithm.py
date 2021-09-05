dc3fab126039421f37e1dd09c0a31b98f7f17f52
teo <teodantes@gmail.com>
2021-09-05 23:44:03 +0300

    Delete odd spaces from euclidean_algorithm.py

--- teo/euclidean_algorithm.py
+++ teo/euclidean_algorithm.py
@@ -2,32 +2,24 @@
 
 def euclid(a, b: int) -> int:
     '''Find the greatest common divisor of given positive integers using Euclidean algorithm.'''
-
     def input_check():
         print('\n\t' + 'Enter two positive integers!' + '\n')
         a = int(input('\t\t> '))
         b = int(input('\t\t> '))
         return euclid(a, b)
-
     if a <= 0 or b <= 0:
         try:
             return input_check()
         except:
             return input_check()
-
     result = 0
-
     if a < b:
         a, b = b, a
-
     if a == b:
         return 1
-
     if a % b == 0:
         return b
-
     result += euclid(b, a % b)
-
     return result
 
 def main():
