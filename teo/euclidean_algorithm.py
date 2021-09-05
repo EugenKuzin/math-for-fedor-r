1e9e219ec64861fe65735e6fd9158d424bb4d1c2
teo <teodantes@gmail.com>
2021-09-05 23:38:51 +0300

    Add Euclidean Algorithm function

---
+++ teo/euclidean_algorithm.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+def euclid(a, b: int) -> int:
+    '''Find the greatest common divisor of given positive integers using Euclidean algorithm.'''
+
+    def input_check():
+        print('\n\t' + 'Enter two positive integers!' + '\n')
+        a = int(input('\t\t> '))
+        b = int(input('\t\t> '))
+        return euclid(a, b)
+
+    if a <= 0 or b <= 0:
+        try:
+            return input_check()
+        except:
+            return input_check()
+
+    result = 0
+
+    if a < b:
+        a, b = b, a
+
+    if a == b:
+        return 1
+
+    if a % b == 0:
+        return b
+
+    result += euclid(b, a % b)
+
+    return result
+
+def main():
+    try:
+        a = int(input('\t\t> '))
+        b = int(input('\t\t> '))
+    except:
+        print('\n\t' + 'Enter two positive integers!' + '\n')
+        return main()
+    print('\n\t' + 'RESULT: ', euclid(a, b), '\n')
+
+if __name__ == '__main__':
+    print('\n\t' + 'Enter two positive integers to find the greatest common divisor of those:' + '\n')
+    main()
