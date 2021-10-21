bee73bb23412d717d73ee6da7434a79722ca5453
teo <teodantes@gmail.com>
2021-10-22 00:41:55 +0300

    Fix error in addition module

--- teo/addition.py
+++ teo/addition.py
@@ -9,17 +9,7 @@ def predecessor(x: int) -> int:
     return x - 1
 
 def sum(a, b: int) -> int:
-    '''Return the sum of given positive integers using recursion.'''
-    if a < 0 or b < 0:
-        print('\n\t' + 'Enter two positive integers!' + '\n')
-        try:
-            a = int(input('\t\t> '))
-            b = int(input('\t\t> '))
-        except:
-            print('\n\t' + 'Enter integers!' + '\n')
-            a = int(input('\t\t> '))
-            b = int(input('\t\t> '))
-            return sum(a, b)
+    '''Return the sum of given non-negative integers using recursion.'''
     return a if b == 0 else successor(sum(a, predecessor(b)))
 
 def main():
@@ -28,6 +18,9 @@ def main():
         b = int(input('\t\t> '))
     except:
         print('\n\t' + 'Enter integers!' + '\n')
+        return main()
+    if a < 0 or b < 0:
+        print('\n\t' + 'Enter non-negative integers!' + '\n')
         return main()
     print('\n\t' + 'RESULT: ', sum(a, b), '\n')
 
