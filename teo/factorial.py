bfe5172a372cbfc9f373d29daf4eb510c67d4ee6
teo <teodantes@gmail.com>
2021-10-23 22:33:16 +0300

    Fix errors in factorial module

--- teo/factorial.py
+++ teo/factorial.py
@@ -2,15 +2,6 @@
 
 def factorial(a: int) -> int:
     '''Multiply a given integer by every integer below it.'''
-    if a < 0:
-        print('\n\t' + 'An integer should be non-negative!' + '\n')
-        try:
-            a = int(input('\t\t> '))
-            return factorial(a)
-        except:
-            print('\n\t' + 'Enter an integer!' + '\n')
-            a = int(input('\t\t> '))
-            return factorial(a)
     if a == 0:
         return 1
     result = a * factorial(a - 1)
@@ -22,8 +13,14 @@ def main():
     except:
         print('\n\t' + 'Enter an integer!' + '\n')
         return main()
+    if a < 0:
+        print('\n\t' + 'Enter non-negative integers!' + '\n')
+        return main()
     print('\n\t' + 'RESULT: ', factorial(a), '\n')
 
-if __name__ == '__main__':
+def start():
     print('\n\t' + 'Enter a non-negative integer to perform factorial calculations:' + '\n')
     main()
+
+if __name__ == '__main__':
+    start()
