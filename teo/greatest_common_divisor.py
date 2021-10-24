50ca3bc180458a5808d90b7b7e9f9a39e0722038
teo <teodantes@gmail.com>
2021-10-24 13:54:31 +0300

    Fix greatest common divisor module

--- teo/greatest_common_divisor.py
+++ teo/greatest_common_divisor.py
@@ -30,10 +30,16 @@ def main():
         a = int(input('\t\t> '))
         b = int(input('\t\t> '))
     except:
+        print('\n\t' + 'Enter integers!' + '\n')
+        return main()
+    if a <= 0 or b <= 0:
         print('\n\t' + 'Enter two positive integers!' + '\n')
         return main()
     print('\n\t' + 'RESULT: ', gcd(a, b), '\n')
 
-if __name__ == '__main__':
+def start():
     print('\n\t' + 'Enter two positive integers to find the greatest common divisor of those:' + '\n')
     main()
+
+if __name__ == '__main__':
+    start()
